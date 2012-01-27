a447039e25ccf7d1ea1563e832d6688c1376f988
jacob.valenta@gmail.com <jacob.valenta@gmail.com@3616650b-56d2-7058-01ee-3ccf921b8187>
2012-01-27 13:38:43 +0000

    added the start of IRC support

--- trunk/client.py
+++ trunk/client.py
@@ -1,11 +1,20 @@
 import socket
 
 class Client():
-	def __init__(clientType="default"):
+	def __init__(self, serverIp="193.219.128.49", serverPort=6667, clientType="irc"):
+		if clientType == "irc":
+			self.clientType = "irc"
+			self.serverIp = serverIp
+			self.serverPort = serverPort
+			self.initIrc()
+
+	def initXmpp(self):
 		pass
 
-	def initXmpp():
-		pass
+	def initIrc(self):
+		self.tcpsoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.tcpsoc.connect((self.serverIp, self.serverPort))
+		self.tcpsoc.close()
 
 if __name__ == '__main__':
-	connType = input("1. Google talk\r\n")
+	client = Client()
